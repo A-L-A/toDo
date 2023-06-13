09dd83597ccd708bfe080afe015ff1ab165e2f60
A-L-A <64477195+A-L-A@users.noreply.github.com>
2023-06-13 19:21:19 +0200

    complete to do list on backend

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+console.log(date());
 
 const app = express();
 
-var items = ["Shop", "Work"];
+const  items = ["Shop", "Work"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
+ 
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-   };
-
-   var day = today.toLocaleDateString("en-US", options);
+  const day = date();
 
   res.render("list", {listTitle: day, newListItems: items});
 
@@ -30,19 +26,28 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  var item = req.body.newItem;
+    const item = req.body.newItem;
 
-  items.push(item);
-
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
     res.redirect("/");
-})
+  }
+});
 
 app.get("/work", function(req, res){
   res.render("list", {listTitle: "Work List", newListItems: workItems})
 });
 
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
 app.post("/work", function(req, res){
-  let item =req.body.newItem;
+  console.log(req.body);
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work"); 
 
